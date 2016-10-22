191b7de5f6a55dcb7600dd38b826c92bc21d5b8c
Thomas & Lisa <pair+TJRobson.lisa-seeto@devbootcamp.com>
2016-10-22 20:58:04 +1300

    Updates

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 const feathers = require('feathers-client')
 const io = require('socket.io-client')
 
-const socket = io('http://192.168.1.8:3030/')
+const socket = io('https://coffee-cloud.herokuapp.com/')
 
 var api = feathers()
 .configure(feathers.hooks())
