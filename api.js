379c78ed4f16ab334a207cc89755a7af5287379c
Thomas & Lisa <pair+TJRobson.lisa-seeto@devbootcamp.com>
2016-10-22 22:15:03 +1300

    Updates

--- api.js
+++ api.js
@@ -1,13 +1,13 @@
 const feathers = require('feathers-client')
 const io = require('socket.io-client')
 
-const socket = io('https://coffee-cloud.herokuapp.com/')
+const socket = io('http://192.168.1.8:3030/')
 
 var api = feathers()
 .configure(feathers.hooks())
 .configure(feathers.socketio(socket));
 
-var orderService = api.service('dumb-orders');
+var orderService = api.service('orders');
 
 // orderService.on('created', function(order) {
 //   console.log('Someone created an order from api', order);
