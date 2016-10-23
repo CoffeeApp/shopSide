e52bd432196e1053d63e552733e0fcd5d3d62dfc
Thomas & Lisa <pair+TJRobson.lisa-seeto@devbootcamp.com>
2016-10-23 16:40:43 +1300

    passing 14 tests

--- components/orderStatus.js
+++ components/orderStatus.js
@@ -13,7 +13,7 @@ class OrderStatus extends Component {
   render() {
     return (
       <div>
-        <button id ={this.props.order_id} onClick= {this.handleUpdate} value="start">start</button>
+        <button id ={this.props.order_id} onClick= {this.handleUpdate} value="started">start</button>
         <button id ={this.props.order_id} onClick = {this.handleUpdate} value="ready for collection">ready</button>
         <button id ={this.props.order_id} onClick = {this.handleUpdate} value="complete">complete</button>
       </div>
