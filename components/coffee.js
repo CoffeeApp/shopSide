102c7e8567830f6a1bb1698d7293a7d5a1363246
Thomas & Lisa <pair+TJRobson.lisa-seeto@devbootcamp.com>
2016-10-25 11:37:55 +1300

    add note and format coffee for readability

--- components/coffee.js
+++ components/coffee.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 function Coffee (props) {
-  const {qty, sugar, milk, type} = props
+  const {quantity, sugar, milk, type, note} = props
   return (
     <div style={{
       background: '#3f0000',
@@ -9,9 +9,13 @@ function Coffee (props) {
       padding:'50px',
       paddingLeft:'40px'
     }}>
-      <h3>{qty} {qty > 1 ? type + 's' : type}</h3>
-      <h3>{sugar} {sugar > 1 ? 'sugars' : 'sugar'} each</h3>
-      <h3> {qty > 1 ? 'all with' : 'with'} {milk} milk</h3>
+      <h3>{quantity} {quantity > 1 ? type + 's' : type}</h3>
+      <ul style={{listStyleType: 'none'}}>
+        <li><h3>{sugar > 0 ? (sugar > 1 ? `${sugar} sugars each` : `${sugar} sugar each`) : ''} </h3></li>
+        <li><h3> {quantity > 1 ? 'all with' : 'with'} {milk} milk</h3></li>
+        <li><h3> { note && note.length > 0 ? `Note: ${note}` : ''}</h3></li>
+      </ul>
+      <hr />
     </div>
   )
 }
